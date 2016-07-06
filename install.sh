639932287f6c835a2f5786838b8de84ea08bce16
Colby Sollars <colby@nucleartuxedo.com>
2016-07-05 23:31:35 -0500

    Added autoclose

--- install.sh
+++ install.sh
@@ -3,10 +3,11 @@
 cd $(dirname $0)
 INSTALLPATH=$(pwd)
 echo "INSTALL PATH: $INSTALLPATH"
-mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle
+mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle $HOME/.vim/plugin
 wget https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim -O $HOME/.vim/autoload/pathogen.vim
 cd $HOME/.vim/bundle
 git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/ctrlpvim/ctrlp.vim.git
+wget http://www.vim.org/scripts/download_script.php?src_id=9223 -O ../plugin/autoclose.vim
 cat $INSTALLPATH/vimrc >> $HOME/.vimrc
 echo "set runtimepath^=~/.vim/bundle/ctrlp.vim" >> $HOME/.vimrc
--- vimrc
+++ vimrc
@@ -8,3 +8,4 @@ nnoremap <C-J> <C-W><C-J>
 nnoremap <C-K> <C-W><C-K>
 nnoremap <C-L> <C-W><C-L>
 nnoremap <C-H> <C-W><C-H>
+let s:running = 1
