21dbd03d95ed84b3f603550145f708c6e0009af4
Colby Sollars <colby@nucleartuxedo.com>
2016-07-07 09:44:33 -0500

    Added themes and 256 color support

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ cd $(dirname $0)
 INSTALLPATH=$(pwd)
 echo "INSTALL PATH: $INSTALLPATH"
 mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle $HOME/.vim/plugin
+cp -r $INSTALLPATH/colors $HOME/.vim/
 wget https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim -O $HOME/.vim/autoload/pathogen.vim
 cd $HOME/.vim/bundle
 git clone https://github.com/scrooloose/nerdtree.git
